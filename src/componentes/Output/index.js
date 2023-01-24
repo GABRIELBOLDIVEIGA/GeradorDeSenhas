8f54adc28feed08eb30279ae7f2ec428ff15b6e9
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-01-24 19:02:16 -0300

    ...

--- src/componentes/Output/index.js
+++ src/componentes/Output/index.js
@@ -4,12 +4,17 @@ import copy from "copy-to-clipboard";
 export default function Output({ senha, placeholder }) {
     const copiar = () => {
         copy(senha);
+        console.log("Texto copiado: " + senha);
     };
 
     return (
-        <div className="senha-gerada">
-            {<p>{senha ? senha : placeholder}</p>}
-            <button onClick={copiar}></button>
-        </div>
+        <>
+            <div className="senha-gerada">
+                {<p>{senha ? senha : placeholder}</p>}
+                <button onClick={copiar}></button>
+            </div>
+            {/* <span className="span-alert">Senha Copiada</span> */}
+        </>
     );
 }
+
